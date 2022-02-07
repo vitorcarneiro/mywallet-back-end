a5e74af764e441b14d5d70f1f97abce077c497c5
vitorcarneiro <vitorcarneiro.sdo@gmail.com>
2022-02-06 23:48:57 -0300

    fix: use cors()

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes/index.js';
 
 const server = express();
-server.use(express.json());
+server.use(express.json(), cors(), router);
 
 server.use(router);
 
